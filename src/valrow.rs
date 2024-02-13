59fee7b6553d51feadb0fed25ccdb1e1c67b5a27
MaulingMonkey <git@maulingmonkey.com>
2024-02-12 16:38:55 -0800

    misc. formatting

--- src/valrow.rs
+++ src/valrow.rs
@@ -4,6 +4,7 @@ use core::cmp::Ordering;
 use core::fmt::{self, Debug, Display, Formatter};
 use core::hash::{Hash, Hasher};
 use core::marker::PhantomData;
+use core::mem::{align_of, size_of, transmute};
 use core::ops::Deref;
 
 
@@ -11,39 +12,38 @@ use core::ops::Deref;
 /// A by-value borrow.
 pub struct Valrow<'a, T: Borrowable>(T::Abi, PhantomData<&'a T>);
 
-unsafe impl<'a, T: Borrowable   > Send          for Valrow<'a, T> where &'a T : Send {}
-unsafe impl<'a, T: Borrowable   > Sync          for Valrow<'a, T> where &'a T : Sync {}
-impl<T: Borrowable              > Copy          for Valrow<'_, T> {}
-impl<T: Borrowable              > Clone         for Valrow<'_, T> { fn clone(&self)                        -> Self             { Self(self.0, self.1) }                            }
-impl<T: Borrowable              > AsRef<T>      for Valrow<'_, T> { fn as_ref(&self)                       -> &T               { Self::as_ref(self) }                              }
-impl<T: Borrowable              > Deref         for Valrow<'_, T> { fn deref(&self)                        -> &T               { Self::as_ref(self) } type Target = T;             }
-impl<'a, T: Borrowable          > From<&'a mut T> for Valrow<'a, T> { fn from(reference: &'a mut T) -> Self { Self::new(reference) } }
-impl<'a, T: Borrowable          > From<&'a     T> for Valrow<'a, T> { fn from(reference: &'a     T) -> Self { Self::new(reference) } }
+unsafe impl<'a, T: Borrowable   > Send              for Valrow<'a, T> where &'a T : Send {}
+unsafe impl<'a, T: Borrowable   > Sync              for Valrow<'a, T> where &'a T : Sync {}
+impl<    T: Borrowable          > Copy              for Valrow<'_, T> {}
+impl<    T: Borrowable          > Clone             for Valrow<'_, T> { fn clone(&self)                        -> Self             { Self(self.0, self.1) }                            }
+impl<    T: Borrowable          > AsRef<T>          for Valrow<'_, T> { fn as_ref(&self)                       -> &T               { Self::as_ref(self) }                              }
+impl<    T: Borrowable          > Deref             for Valrow<'_, T> { fn deref(&self)                        -> &T               { Self::as_ref(self) } type Target = T;             }
+impl<'a, T: Borrowable          > From<&'a mut T>   for Valrow<'a, T> { fn from(reference: &'a mut T) -> Self { Self::new(reference) } }
+impl<'a, T: Borrowable          > From<&'a     T>   for Valrow<'a, T> { fn from(reference: &'a     T) -> Self { Self::new(reference) } }
 
 // XXX: actually, how many of these traits are really needed?
-impl<T: Borrowable + Debug      > Debug         for Valrow<'_, T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result      { <T as Debug       >::fmt(self, fmt) }             }
-impl<T: Borrowable + Display    > Display       for Valrow<'_, T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result      { <T as Display     >::fmt(self, fmt) }             }
-impl<T: Borrowable + PartialEq  > PartialEq     for Valrow<'_, T> { fn eq(&self, other: &Self)             -> bool             { <T as PartialEq   >::eq(self, other) }            }
-impl<T: Borrowable + Eq         > Eq            for Valrow<'_, T> {}
-impl<T: Borrowable + PartialOrd > PartialOrd    for Valrow<'_, T> { fn partial_cmp(&self, other: &Self)    -> Option<Ordering> { <T as PartialOrd  >::partial_cmp(self, other) }   }
-impl<T: Borrowable + Ord        > Ord           for Valrow<'_, T> { fn cmp(&self, other: &Self)            -> Ordering         { <T as Ord         >::cmp(self, other) }           }
-impl<T: Borrowable + Hash       > Hash          for Valrow<'_, T> { fn hash<H: Hasher>(&self, state: &mut H)                   { <T as Hash        >::hash(self, state) }          }
+impl<T: Borrowable + Debug      > Debug             for Valrow<'_, T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result      { <T as Debug       >::fmt(self, fmt) }             }
+impl<T: Borrowable + Display    > Display           for Valrow<'_, T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result      { <T as Display     >::fmt(self, fmt) }             }
+impl<T: Borrowable + PartialEq  > PartialEq         for Valrow<'_, T> { fn eq(&self, other: &Self)             -> bool             { <T as PartialEq   >::eq(self, other) }            }
+impl<T: Borrowable + Eq         > Eq                for Valrow<'_, T> {}
+impl<T: Borrowable + PartialOrd > PartialOrd        for Valrow<'_, T> { fn partial_cmp(&self, other: &Self)    -> Option<Ordering> { <T as PartialOrd  >::partial_cmp(self, other) }   }
+impl<T: Borrowable + Ord        > Ord               for Valrow<'_, T> { fn cmp(&self, other: &Self)            -> Ordering         { <T as Ord         >::cmp(self, other) }           }
+impl<T: Borrowable + Hash       > Hash              for Valrow<'_, T> { fn hash<H: Hasher>(&self, state: &mut H)                   { <T as Hash        >::hash(self, state) }          }
 
 impl<'a, T: Borrowable> Valrow<'a, T> {
     /// Borrow `*reference` by value.
     #[inline(always)] pub const fn new(reference: &'a T) -> Self {
         let _ = Self::STATIC_CHECK_T_ABI;
-        Self(unsafe { *core::mem::transmute::<&T, &T::Abi>(reference) }, PhantomData)
+        Self(unsafe { *transmute::<&T, &T::Abi>(reference) }, PhantomData)
     }
 
     fn as_ref(&self) -> &T {
         let _ = Self::STATIC_CHECK_T_ABI;
-        unsafe { core::mem::transmute(self) }
+        unsafe { transmute(self) }
     }
 
     /// Makes an effort to validate `T` and `T::Abi` are compatible.  This may have false negatives (e.g. fail to trip despite incompatability.)
     const STATIC_CHECK_T_ABI : () = {
-        use core::mem::*;
         assert!(align_of::<       T >() == align_of::<T::Abi>());
         assert!( size_of::<       T >() ==  size_of::<T::Abi>());
         assert!(align_of::<Option<T>>() == align_of::<Option<T::Abi>>()); // imperfect check for niche compatability
--- src/valrow_mut.rs
+++ src/valrow_mut.rs
@@ -4,6 +4,7 @@ use core::cmp::Ordering;
 use core::fmt::{self, Debug, Display, Formatter};
 use core::hash::{Hash, Hasher};
 use core::marker::PhantomData;
+use core::mem::{align_of, size_of, transmute};
 use core::ops::{Deref, DerefMut};
 
 
@@ -11,43 +12,42 @@ use core::ops::{Deref, DerefMut};
 /// A by-value mutable/exclusive borrow.  Only usable for Zero Sized Types.  Only useful if <code>\![Copy]</code>.
 pub struct ValrowMut<'a, T: Borrowable>(T::Abi, PhantomData<&'a mut T>);
 
-unsafe impl<'a, T: Borrowable   > Send          for ValrowMut<'a, T> where &'a mut T : Send {}
-unsafe impl<'a, T: Borrowable   > Sync          for ValrowMut<'a, T> where &'a mut T : Sync {}
-impl<T: Borrowable              > AsRef<T>      for ValrowMut<'_, T> { fn as_ref(&    self)                   -> &    T           { Self::as_ref(self) }                              }
-impl<T: Borrowable              > AsMut<T>      for ValrowMut<'_, T> { fn as_mut(&mut self)                   -> &mut T           { Self::as_mut(self) }                              }
-impl<T: Borrowable              > Deref         for ValrowMut<'_, T> { fn deref    (&    self)                -> &    T           { Self::as_ref(self) } type Target = T;             }
-impl<T: Borrowable              > DerefMut      for ValrowMut<'_, T> { fn deref_mut(&mut self)                -> &mut T           { Self::as_mut(self) }                              }
-impl<'a, T: Borrowable          > From<&'a mut T> for ValrowMut<'a, T> { fn from(reference: &'a mut T) -> Self { Self::new(reference) } }
+unsafe impl<'a, T: Borrowable   > Send              for ValrowMut<'a, T> where &'a mut T : Send {}
+unsafe impl<'a, T: Borrowable   > Sync              for ValrowMut<'a, T> where &'a mut T : Sync {}
+impl<    T: Borrowable          > AsRef<T>          for ValrowMut<'_, T> { fn as_ref(&    self)                   -> &    T           { Self::as_ref(self) }                              }
+impl<    T: Borrowable          > AsMut<T>          for ValrowMut<'_, T> { fn as_mut(&mut self)                   -> &mut T           { Self::as_mut(self) }                              }
+impl<    T: Borrowable          > Deref             for ValrowMut<'_, T> { fn deref    (&    self)                -> &    T           { Self::as_ref(self) } type Target = T;             }
+impl<    T: Borrowable          > DerefMut          for ValrowMut<'_, T> { fn deref_mut(&mut self)                -> &mut T           { Self::as_mut(self) }                              }
+impl<'a, T: Borrowable          > From<&'a mut T>   for ValrowMut<'a, T> { fn from(reference: &'a mut T) -> Self { Self::new(reference) } }
 
 // XXX: actually, how many of these traits are really needed?
-impl<T: Borrowable + Debug      > Debug         for ValrowMut<'_, T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result      { <T as Debug       >::fmt(self, fmt) }             }
-impl<T: Borrowable + Display    > Display       for ValrowMut<'_, T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result      { <T as Display     >::fmt(self, fmt) }             }
-impl<T: Borrowable + PartialEq  > PartialEq     for ValrowMut<'_, T> { fn eq(&self, other: &Self)             -> bool             { <T as PartialEq   >::eq(self, other) }            }
-impl<T: Borrowable + Eq         > Eq            for ValrowMut<'_, T> {}
-impl<T: Borrowable + PartialOrd > PartialOrd    for ValrowMut<'_, T> { fn partial_cmp(&self, other: &Self)    -> Option<Ordering> { <T as PartialOrd  >::partial_cmp(self, other) }   }
-impl<T: Borrowable + Ord        > Ord           for ValrowMut<'_, T> { fn cmp(&self, other: &Self)            -> Ordering         { <T as Ord         >::cmp(self, other) }           }
-impl<T: Borrowable + Hash       > Hash          for ValrowMut<'_, T> { fn hash<H: Hasher>(&self, state: &mut H)                   { <T as Hash        >::hash(self, state) }          }
+impl<T: Borrowable + Debug      > Debug             for ValrowMut<'_, T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result      { <T as Debug       >::fmt(self, fmt) }             }
+impl<T: Borrowable + Display    > Display           for ValrowMut<'_, T> { fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result      { <T as Display     >::fmt(self, fmt) }             }
+impl<T: Borrowable + PartialEq  > PartialEq         for ValrowMut<'_, T> { fn eq(&self, other: &Self)             -> bool             { <T as PartialEq   >::eq(self, other) }            }
+impl<T: Borrowable + Eq         > Eq                for ValrowMut<'_, T> {}
+impl<T: Borrowable + PartialOrd > PartialOrd        for ValrowMut<'_, T> { fn partial_cmp(&self, other: &Self)    -> Option<Ordering> { <T as PartialOrd  >::partial_cmp(self, other) }   }
+impl<T: Borrowable + Ord        > Ord               for ValrowMut<'_, T> { fn cmp(&self, other: &Self)            -> Ordering         { <T as Ord         >::cmp(self, other) }           }
+impl<T: Borrowable + Hash       > Hash              for ValrowMut<'_, T> { fn hash<H: Hasher>(&self, state: &mut H)                   { <T as Hash        >::hash(self, state) }          }
 
 impl<'a, T: Borrowable> ValrowMut<'a, T> {
     /// Borrow `*reference` by value.
     #[inline(always)] pub fn new(reference: &'a mut T) -> Self {
         let _ = Self::STATIC_CHECK_T_ABI;
-        Self(unsafe { *core::mem::transmute::<&mut T, &mut T::Abi>(reference) }, PhantomData)
+        Self(unsafe { *transmute::<&mut T, &mut T::Abi>(reference) }, PhantomData)
     }
 
     fn as_ref(&self) -> &T {
         let _ = Self::STATIC_CHECK_T_ABI;
-        unsafe { core::mem::transmute(self) }
+        unsafe { transmute(self) }
     }
 
     fn as_mut(&mut self) -> &mut T {
         let _ = Self::STATIC_CHECK_T_ABI;
-        unsafe { core::mem::transmute(self) }
+        unsafe { transmute(self) }
     }
 
     /// Makes an effort to validate `T` and `T::Abi` are compatible.  This should have no false negatives?
     const STATIC_CHECK_T_ABI : () = {
-        use core::mem::*;
         assert!(0               ==  size_of::<T     >()); // T      *must* be a ZST
         assert!(0               ==  size_of::<T::Abi>()); // T::Abi *must* be a ZST
         assert!(align_of::<T>() == align_of::<T::Abi>());

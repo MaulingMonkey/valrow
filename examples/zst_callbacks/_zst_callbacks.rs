52aa2b7b3a8688683d938c79cb6b416d0f8c0ddc
MaulingMonkey <git@maulingmonkey.com>
2024-02-12 17:15:26 -0800

    examples/zst_callbacks: fix docs, we weren't leaking callback anymore

--- examples/zst_callbacks/_zst_callbacks.rs
+++ examples/zst_callbacks/_zst_callbacks.rs
@@ -11,20 +11,20 @@
 //! let zst_borrow  = Valrow::new(&zst);
 //!
 //! # #[cfg(wont_compile)] // rejected by StaticAssert::<Callback>::IS_ZST
-//! call_me_back_asap(move || { dbg!(zst_ref);    }); // ❌ won't compile: `zst_ref` isn't a ZST
-//! call_me_back_asap(move || { dbg!(zst_borrow); }); // ✔️ will compile: `zst_borrow` *is* a ZST
+//! call_callback(move || { dbg!(zst_ref);    }); // ❌ won't compile: `zst_ref` isn't a ZST
+//! call_callback(move || { dbg!(zst_borrow); }); // ✔️ will compile: `zst_borrow` *is* a ZST
 //!
-//! fn call_me_back_asap<Callback: FnMut()>(mut callback: Callback) {
+//! fn call_callback<Callback: FnMut()>(mut callback: Callback) {
 //!     let callback = &mut callback;
-//!     unsafe { call_me_back_asap(adapt::<Callback>) };
+//!     unsafe { call_callback(adapt::<Callback>) };
 //!     let _ = callback;
 //!
 //! #   #[cfg(nope)] {
-//!     #[link(name = "clibrary")] extern "C" { fn call_me_back_asap(callback: extern "C" fn()); }
+//!     #[link(name = "clibrary")] extern "C" { fn call_callback(callback: extern "C" fn()); }
 //! #   }
-//! #   unsafe fn call_me_back_asap(callback: extern "C" fn()) { callback() }
+//! #   unsafe fn call_callback(callback: extern "C" fn()) { callback() }
 //!     extern "C" fn adapt<Callback: FnMut()>() {
-//!         // we leaked a `Callback` earlier, so assume we can make a reference to it:
+//!         // ⚠️ assume only `call_callback` calls `adapt` (has a `&mut Callback` on the stack)
 //!         let _ = StaticAssert::<Callback>::IS_ZST; // otherwise we need a real address
 //!         let callback = unsafe { core::ptr::NonNull::<Callback>::dangling().as_mut() };
 //!         callback();
